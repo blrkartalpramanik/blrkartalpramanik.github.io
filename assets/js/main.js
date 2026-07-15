7898f235674c8bdb8bc95b77805a4d268eddafc0
blrkartalpramanik <87308624+blrkartalpramanik@users.noreply.github.com>
2026-07-15 13:59:29 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 
+fetch("components/topbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("topbar").innerHTML = data;
+    });
+fetch("components/header.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    });
+fetch("components/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar-container").innerHTML = data;
+    });
+
+fetch("components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    });
